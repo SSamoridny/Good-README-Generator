2b9b703ca61ac78b5e3e8753e9c02f88ee90e3da
Shaun Samoridny <ssamoridny@gmail.com>
2020-10-12 11:02:29 -0600

    I worked on this and I am close but I am having an issue with the licenses

---
+++ Develop/index.js
@@ -0,0 +1,110 @@
+// Dependencies 
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const path = require('path');
+
+// User questions
+function promptUser() {
+ return inquirer.prompt([
+            {
+            name: "project_title",
+            type: "input",
+            message: "What is your project called?",
+        } ,
+        {
+            name:"project_description",
+            type: "input",
+            message: "Write a brief description of the project."
+        },
+        {
+            name:"installation_instructions",
+            type: "input",
+            message: "How do you install your project?"
+        },
+        {
+            name: "usage_instructions",
+            type: "input",
+            message: "Please tell me how to use the product.",
+        },
+        {
+            name: "project_credits",
+            type: "input" ,
+            message: "Please list collaborators for the project and their contributions."
+
+        },
+        {
+            name: "project_license",
+            type: "checkbox",
+            message: "Please select one license for this project",
+            choices: [
+                "Apache",
+                "MIT",
+                "ISC",
+                "GNU GPL v3"
+            ]
+        },
+       
+
+    ]);
+}
+
+
+
+// This part actually takes the responses and creates our README file using backtick notation and some clever spacing
+
+function generateMarkdown(response){
+
+        return`
+
+# ${response.project_title}
+![](https://img.shields.io/badge/Readme-100%25-green)
+
+## Description: 
+
+         ${response.project_description}
+
+## Installation:
+
+         ${response.installation_instructions}
+
+## Usage:
+
+         ${response.usage_instructions}
+
+## Credits:
+
+        ${response.project_credits}
+
+## License:
+    
+
+    
+    ${getLicense(response.project_license)}
+      
+
+   
+`;
+}
+// After taking in the information, this is the function that creates our README by generating a README.md file and wriring the answers given to that file
+  function generate(){
+      promptUser()
+      .then(function(response) {
+          let readMe = generateMarkdown(response);
+          fs.writeFileSync("README.md" , readMe)
+      })
+      .catch(function(err){
+          console.log("---Error---" , err)
+
+      })
+  }
+// this calls the function
+generate();
+
+
+
+
+
+
+
+
+
